197e9d0fed089925bedca73590c3d228ecb0319c
AswinAzikar <azwinaswin8@gmail.com>
2024-05-23 06:49:30 +0530

    added a HomePAge to mention navbar for explicit pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,21 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:turfnest_admin/Dashboard.dart';
-import 'package:turfnest_admin/Menu.dart';
-import 'package:turfnest_admin/constants.dart';
+import 'package:turfnest_admin/SignUp.dart';
+import 'package:turfnest_admin/HomeScreen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  MyApp({super.key});
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final _pageController = PageController();
-  int _pageIndex = 0;
-
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    double screenHeight = MediaQuery.of(context).size.height;
-    double screenWidth = MediaQuery.of(context).size.width;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: PageView(
-          controller: _pageController,
-          onPageChanged: (index) {
-            setState(() {
-              _pageIndex = index;
-            });
-          },
-          children: const <Widget>[
-            Dashboard(),
-            Menu(),
-          ],
-        ),
-        bottomNavigationBar: CurvedNavigationBar(
-          backgroundColor: AppColors.scbgd,
-          buttonBackgroundColor: AppColors.blue,
-          color: AppColors.blue,
-          height: 0.05 * screenHeight,
-          animationCurve: Curves.easeIn,
-          items: const <Widget>[
-            Icon(
-              Icons.dashboard_outlined,
-              size: 35,
-              color: AppColors.white,
-            ),
-            Icon(
-              Icons.menu_open_sharp,
-              size: 34,
-              color: AppColors.white,
-            ),
-          ],
-          index: _pageIndex,
-          onTap: (index) {
-            _pageController.animateToPage(index,
-                duration: const Duration(milliseconds: 300),
-                curve: Curves.easeOut);
-          },
-        ),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SignUpPage(),
+        '/home': (context) => HomeScreen(),
+      },
     );
   }
 }
